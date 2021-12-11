ba2b3df3a03fca745ec85efdbc689ff2572f0a9d
Ilya Dzyuin <ilua.dzuin@gmail.com>
2021-12-11 16:56:08 +0400

    init: add configs and src template

---
+++ christmas-task/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: path.resolve(__dirname, './src/index.ts'),
+  output: {
+    hashDigestLength: 6,
+    filename: '[name]-[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html',
+    }),
+    new CleanWebpackPlugin(),
+    // new CopyWebpackPlugin({
+    //   patterns: [
+    //     {
+    //       from: path.resolve(__dirname, 'src/assets'),
+    //       to: path.resolve(__dirname, 'dist/assets'),
+    //     },
+    //   ],
+    // }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: { url: false },
+          },
+        ],
+      },
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+};
